f61f93b7ac29cffa72375f530af37598a812d213
Samweli <smwltwesa6@gmail.com>
2024-11-01 15:11:36 +0300

    use the renamed requirements files

--- scripts/docker/geosys-test-pre-scripts.sh
+++ scripts/docker/geosys-test-pre-scripts.sh
@@ -7,5 +7,5 @@ rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/stream_fea
 ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/stream_feature_extractor
 ln -sf /tests_directory /usr/share/qgis/python/plugins/stream_feature_extractor
 
-pip3 install -r /tests_directory/REQUIREMENTS.txt
-pip3 install -r /tests_directory/REQUIREMENTS_TESTING.txt
+pip3 install -r /tests_directory/requirements.txt
+pip3 install -r /tests_directory/requirements_testing.txt
