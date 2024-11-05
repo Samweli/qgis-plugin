4a0f860322f55e71e05ecfcc27b01aef74e94633
Samweli <smwltwesa6@gmail.com>
2024-11-05 14:26:51 +0300

    workflow for staging repository

--- admin.py
+++ admin.py
@@ -269,6 +269,125 @@ def _log(
     if verbose:
         typer.echo(msg, *args, **kwargs)
 
+app.command()
+def generate_plugin_repo_xml(
+        context: typer.Context,
+        prerelease=False,
+        prerelease_url=None,
+        prerelease_time=None,
+        prerelease_filename=None,
+):
+    """Generates the plugin repository xml file, from which users
+        can use to install the plugin in QGIS.
+
+    :param context: Application context
+    :type context: typer.Context
+    """
+    repo_base_dir = LOCAL_ROOT_DIR / "docs" / "repository"
+    repo_base_dir.mkdir(parents=True, exist_ok=True)
+    metadata = _get_metadata(context)
+    fragment_template = """
+                <pyqgis_plugin name="{name}" version="{version}">
+                    <description><![CDATA[{description}]]></description>
+                    <about><![CDATA[{about}]]></about>
+                    <version>{version}</version>
+                    <qgis_minimum_version>{qgis_minimum_version}</qgis_minimum_version>
+                    <homepage><![CDATA[{homepage}]]></homepage>
+                    <file_name>{filename}</file_name>
+                    <icon>{icon}</icon>
+                    <author_name><![CDATA[{author}]]></author_name>
+                    <download_url>{download_url}</download_url>
+                    <update_date>{update_date}</update_date>
+                    <experimental>{experimental}</experimental>
+                    <deprecated>{deprecated}</deprecated>
+                    <tracker><![CDATA[{tracker}]]></tracker>
+                    <repository><![CDATA[{repository}]]></repository>
+                    <tags><![CDATA[{tags}]]></tags>
+                    <server>False</server>
+                </pyqgis_plugin>
+        """.strip()
+    contents = "<?xml version = '1.0' encoding = 'UTF-8'?>\n<plugins>"
+    if prerelease:
+        all_releases = [
+            {
+                "pre_release": prerelease,
+                "tag_name": metadata.get("version"),
+                "url": prerelease_url,
+                "published_at": dt.datetime.strptime(
+                    prerelease_time, "%Y-%m-%dT%H:%M:%SZ"
+                ),
+            }
+        ]
+    else:
+        all_releases = _get_existing_releases(context)
+
+    if prerelease:
+        target_releases = all_releases
+    else:
+        target_releases = _get_latest_releases(all_releases)
+
+    for release in [r for r in target_releases if r is not None]:
+        fragment = fragment_template.format(
+            name=metadata.get("name"),
+            version=metadata.get("version"),
+            description=metadata.get("description"),
+            about=metadata.get("about"),
+            qgis_minimum_version=metadata.get("qgisMinimumVersion"),
+            homepage=metadata.get("homepage"),
+            filename=release.get("url").rpartition("/")[-1]
+            if not prerelease_filename
+            else prerelease_filename,
+            icon=metadata.get("icon", ""),
+            author="test",
+            download_url=release.get("url"),
+            update_date=release.get("published_at"),
+            experimental=False,
+            deprecated=metadata.get("deprecated"),
+            tracker=metadata.get("tracker"),
+            repository=metadata.get("repository"),
+            tags=metadata.get("tags"),
+        )
+        contents = "\n".join((contents, fragment))
+    contents = "\n".join((contents, "</plugins>"))
+    repo_index = repo_base_dir / "plugins.xml"
+    repo_index.write_text(contents, encoding="utf-8")
+
+    return contents
+
+
+def _get_metadata(context: typer.Context,):
+    """Reads the metadata properties from the
+       project metadata file 'metadata.txt'.
+
+    :return: plugin metadata
+    :type: Dict
+    """
+    metadata = {}
+    metadata_path = LOCAL_ROOT_DIR / "metadata.txt"
+
+    with open(str(metadata_path), "r") as fh:
+        for line in fh:
+            # Skip empty lines
+            line = line.strip()
+            if not line:
+                continue
+
+            # Split key and value
+            if "=" not in line:
+                continue
+            key, value = line.split("=", 1)
+            metadata[key.strip()] = value.strip()
+
+    # Update metadata with additional derived fields
+    metadata.update(
+        {
+            "tags": metadata.get("tags", "").split(", ") if "tags" in metadata else [],
+            "changelog": metadata.get("changelog", ""),
+        }
+    )
+
+    return metadata
+
 
 def _get_existing_releases(
         context: typing.Optional = None,
@@ -282,7 +401,7 @@ def _get_existing_releases(
     :rtype: List[GithubRelease]
     """
     base_url = "https://api.github.com/repos/" \
-               "GEOSYS/qgis-plugin/releases"
+               "Samweli/qgis-plugin/releases"
     response = httpx.get(base_url)
     result = []
     if response.status_code == 200:
